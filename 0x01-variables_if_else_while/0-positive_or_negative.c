3456a6dd601164fb886f2320e99000bb8fc56698
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-08 16:03:06 -0700

    Any positive number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there*/
+	/*your code goes there*/
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
