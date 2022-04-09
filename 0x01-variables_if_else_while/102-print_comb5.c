4c26469b188ca3d19c511ceebfd963f56d824dd7
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-09 05:06:58 -0700

    Combo of two-two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main -main block
+ * Description: print all possible combinations of two two digits
+ * ranging from 0 to 99
+ * separated by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * The combination of numbers must be separated by comma, followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * 00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int l_d;
+
+	int c2;
+	int f_d2;
+	int l_d2;
+
+	while (c <= 98)
+	{
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
+		{
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
+
+			if (c < c2)
+			{
+				putchar(f_d);
+				putchar(l_d);
+				putchar(' ');
+
+				putchar(f_d2);
+
+				putchar(l_d2);
+
+				if (c != 98)
+				{
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+			c2++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
