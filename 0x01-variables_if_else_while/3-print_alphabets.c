d4af44ea805a35ffd066624972ac99ef0251d96b
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-08 17:48:45 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*more headers go there*/
+
+/**
+ * main - main block
+ * Description: prins aplhabet in lwoer case, then in upper case, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar (c);
+		c++;
+	}
+
+	putchar ('\n');
+	return (0)
+}
