a432b2c306ff1890ad3f13bd290b3c03e22deeae
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-12 15:24:44 -0700

    Checking the alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * _isalpha - check fo loer case letter
+ * @c : character to check the case
+ * Return 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+}
