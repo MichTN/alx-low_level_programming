bbc84585069873ef4309be2ba3d0ebecc294646d
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-12 14:52:43 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: write a function that print the alphabet in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
