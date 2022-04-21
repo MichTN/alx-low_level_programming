df7f985fb0d88615f5ad6db2d4a53ab4119dfa90
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-20 18:52:24 -0700

    Pointer to int

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifdef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,3 +14,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif /* MAIN_H */
