e5201affe27c4b1e13f8bf05414096bbbd02b6e6
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-04-28 11:07:09 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,10 +20,12 @@ int _sqrt_recursion(int n)
  */
 int squareroot(int n, int i)
 {
-	if (i < 1)
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
 		return (-1);
 	else if (i * i == n)
 		return (i);
 	else
-		return (squareroot(n, i + 1));
+		return (squareroot(n, i - 1));
 }
