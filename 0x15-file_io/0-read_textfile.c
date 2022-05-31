232e07c54ba55183cfff69f225133bba5482a13b
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-05-30 18:39:30 -0700

    edits

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,12 +4,13 @@
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
  * @letters: numbers of letters printed.
+ *
  * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t s, t;
+	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
@@ -24,12 +25,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buf)
 		return (0);
 
-	s = read(fd, buf, letters);
-	t = write(STDOUT_FILENO, buf, s);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
 	free(buf);
 
-	return (t);
+	return (nwr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
